60a30ed6f7f59d17218d1d8f9d01e602088ba1a9
Ariel Guareschi <arielguareschi@gmail.com>
2023-03-01 08:42:27 -0300

    projeto perguntas

---
+++ expenses/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:expenses/components/transaction_user.dart';
+import 'package:flutter/material.dart';
+
+main() => runApp(const ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Despesas Pessoais"),
+      ),
+      body: const Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(
+            child: Card(
+              color: Colors.blue,
+              elevation: 5,
+              child: Text('Grafico'),
+            ),
+          ),
+          TransactionUser(),
+        ],
+      ),
+    );
+  }
+}
