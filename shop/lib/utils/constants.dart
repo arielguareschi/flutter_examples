44950551afb928fd3b5c0e53016c6401ca457242
Ariel Guareschi <arielguareschi@gmail.com>
2023-03-29 13:01:09 -0300

    user created

--- shop/lib/utils/constants.dart
+++ shop/lib/utils/constants.dart
@@ -1,6 +1,9 @@
 class Constants {
+  static const userFavoritesUrl =
+      'https://shop-coder-ariel-default-rtdb.firebaseio.com/userFavorites';
   static const productBaseUrl =
       'https://shop-coder-ariel-default-rtdb.firebaseio.com/products';
   static const orderBaseUrl =
       'https://shop-coder-ariel-default-rtdb.firebaseio.com/orders';
+  static const webApiKey = 'AIzaSyAOB9uer_xxRprUZnoZSqeK3oenNmingOc';
 }
