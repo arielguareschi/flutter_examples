5e9694af65417ef18d2e120f6e590211feebf331
Ariel Guareschi <arielguareschi@gmail.com>
2023-04-04 09:53:17 -0300

    greate places

---
+++ greate_places/lib/screen/place_list_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:greate_places/providers/greate_places.dart';
+import 'package:greate_places/utils/app_routes.dart';
+import 'package:provider/provider.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meus Lugares'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(AppRoutes.PLACE_FORM);
+            },
+          ),
+        ],
+      ),
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false).loadPlaces(),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? const Center(child: CircularProgressIndicator())
+            : Consumer<GreatPlaces>(
+                child: const Center(
+                  child: Text('Nenhum local cadastrado'),
+                ),
+                builder: (ctx, greatPlaces, ch) => greatPlaces.itemsCount == 0
+                    ? ch
+                    : ListView.builder(
+                        itemCount: greatPlaces.itemsCount,
+                        itemBuilder: (ctx, i) => ListTile(
+                              leading: CircleAvatar(
+                                backgroundImage: FileImage(
+                                  greatPlaces.itemByIndex(i).image,
+                                ),
+                              ),
+                              title: Text(greatPlaces.itemByIndex(i).title),
+                              subtitle: Text(
+                                  greatPlaces.itemByIndex(i).location.address),
+                              OnTap: () {
+                                Navigator.of(context).pushNamed(
+                                  AppRoutes.PLACE_DETAIL,
+                                  arguments: greatPlaces.itemByIndex(i),
+                                );
+                              },
+                            )),
+              ),
+      ),
+    );
+  }
+}
